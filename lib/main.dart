c2480b599fa6dcb45765c420ede8a7c2123e84a2
singhtaran1005 <singhh3010@gmail.com>
2021-01-16 16:01:31 +0530

    signup_updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:chatify/screens/Home.dart';
+import 'package:chatify/screens/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() async {
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -38,43 +43,44 @@ class _SplashScreenState extends State<SplashScreen> {
 
   void _navigateToHome() {
     Navigator.of(context).pushReplacement(MaterialPageRoute(
-      builder: (BuildContext context) => Home(),
+      builder: (BuildContext context) => SIGNUP(),
     ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-  Stack(
-    fit: StackFit.expand,
-    children: <Widget>[
-      Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF0D4583),
-        ),
-      ),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset('assets/images/n.png',
-                  width: 195,
-                  fit: BoxFit.fitWidth,
-                  alignment: Alignment.topLeft,)
-                ],
-              ),
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF0D4583),
             ),
           ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/n.png',
+                        width: 195,
+                        fit: BoxFit.fitWidth,
+                        alignment: Alignment.topLeft,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
-      )
-    ],
-  ),      
+      ),
     );
   }
 }
